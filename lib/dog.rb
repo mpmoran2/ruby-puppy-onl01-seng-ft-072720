6c1a26eaa552bd13d0ad78bd4173d58336afb9fc
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-06 15:35:19 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,26 @@
-# Add your code here
+class Dog 
+
+  attr_accessor :name 
+  @@all = []
+
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
+
+  def self.all
+    @@all.each do |dog|
+      puts dog.name 
+    end 
+  end  
+
+  def self.clear_all 
+    @@all.clear
+  end 
+  
+  def save
+  end 
+  
+  def self.print_all
+  end
+end 
